a45846969bb249b124a7ed24cf3f5dc722d1d61d
busybox11 <planetgeek11@gmail.com>
2020-10-27 15:30:08 +0100

    Use actual stackoverflow data

--- snip.js
+++ snip.js
@@ -1,21 +1,47 @@
 console.log('hey google')
 
+function httpRequest(url, callback)
+{
+	let xmlHttp = new XMLHttpRequest()
+	
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText)
+	}
+	
+    xmlHttp.open("GET", url, true)
+    xmlHttp.send(null)
+}
+
 for (let i = document.getElementsByClassName('rc').length; i-- > 0;) {
 	let link = document.getElementsByClassName('rc')[i].getElementsByTagName('a')[0]
 	if (link.host == "stackoverflow.com" && link.pathname.startsWith('/questions/')) {
-		console.log(i, link)
-		let type = "Verified solution"
-		let body = "<p>For ID:</p>\n\n<pre><code>var a = document.getElementById('divYouwant').getElementsByTagName('a');\nfor (var i = 0; i &lt; a.length; i++) {\n    var elem = a[i],\n        color = colors[0];\n    elem.style.color = color;\n    colors.push(color);\n    colors.shift();\n}\n</code></pre>\n\n<p>If you want to grab it from a class, you would have to grab each class and then grab each set of anchor tags...</p>\n\n<pre><code>var divs = document.getElementsByClassName('className');\nfor (var i = 0; i &lt; divs.length; i++) {\n    var a = divs[i].getElementsByTagName('a');\n    for (var j = 0; j &lt; a.length; j++) {\n        var elem = a[j],\n            color = colors[0];\n        elem.style.color = color;\n        colors.push(color);\n        colors.shift();\n    }\n}\n</code></pre>\n\n<p>Basically you follow the same concept as just getting all the links. The only difference is you don't use the document as the reference. First you grab the div that you want, and then from there, grab an array of all the anchor tags.</p>\n"
-		let score = 13
+		let id = link.pathname.replace('/questions/', '')
+		id = id.substr(0, id.indexOf('/'))
 
-		document.getElementsByClassName('IsZvec')[i].innerHTML = `
-			<div class="cs-card" id="cs-card-${i}">
-				<div class="cs-card-content">
-					<span class="cs-card-info">${type} - Score: ${score}</span>
+		// Request to StackOverflow's API to get the answers
+		httpRequest(`https://api.stackexchange.com/2.2/questions/${id}/answers?order=desc&sort=votes&site=stackoverflow&filter=!b6AubVkmmBt14D`, function (data) {
+			data = JSON.parse(data)
+			ans = data.items[0]
 
-					<div class="cs-card-answer">${body}</div>
+			let type
+			if (ans.is_accepted) {
+				type = "Verified"
+			} else {
+				type = "Top"
+			}
+			let body = ans.body
+			let score = ans.score
+
+			document.getElementsByClassName('IsZvec')[i].innerHTML = `
+				<div class="cs-card" id="cs-card-${i}">
+					<div class="cs-card-content">
+						<span class="cs-card-info">${type} solution - Score: ${score}</span>
+
+						<div class="cs-card-answer">${body}</div>
+					</div>
 				</div>
-			</div>
-		`
+			`
+		})
 	}
 }
