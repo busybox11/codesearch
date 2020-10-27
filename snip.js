941e1d0b965991c31513a3ff83e69cf37c6f65cd
busybox11 <planetgeek11@gmail.com>
2020-10-27 14:36:07 +0100

    Added sample answer card

--- snip.js
+++ snip.js
@@ -4,6 +4,17 @@ for (let i = document.getElementsByClassName('rc').length; i-- > 0;) {
 	let link = document.getElementsByClassName('rc')[i].getElementsByTagName('a')[0]
 	if (link.host == "stackoverflow.com") {
 		console.log(i, link)
-		document.getElementsByClassName('IsZvec')[i].innerHTML = "Found link<br>" + document.getElementsByClassName('IsZvec')[i].innerHTML
+		let type = "Verified solution"
+		let body = "<p>You can call the array method <strong>map</strong> on the document.links collection.</p>\n\n<p>The String value of a link element its url.</p>\n\n<pre><code>var hrefs= [].map.call(document.links,String);\nalert(hrefs.join('\\n'));\n</code></pre>\n"
+
+		document.getElementsByClassName('IsZvec')[i].innerHTML = `
+			<div class="cs-card" id="cs-card-${i}">
+				<div class="cs-card-content">
+					<span class="cs-card-type">${type}</span>
+
+					<div class="cs-card-answer">${body}</div>
+				</div>
+			</div>
+		`
 	}
 }
