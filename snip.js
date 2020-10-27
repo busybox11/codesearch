1f81992543584f1544d08875dbf0786586803b65
busybox11 <planetgeek11@gmail.com>
2020-10-27 12:14:51 +0100

    Add "Found link" to link content

--- snip.js
+++ snip.js
@@ -4,6 +4,6 @@ for (let i = document.getElementsByClassName('rc').length; i-- > 0;) {
 	let link = document.getElementsByClassName('rc')[i].getElementsByTagName('a')[0]
 	if (link.host == "stackoverflow.com") {
 		console.log(i, link)
-		link.outerHTML += "Found link"
+		document.getElementsByClassName('IsZvec')[i].innerHTML = "Found link<br>" + document.getElementsByClassName('IsZvec')[i].innerHTML
 	}
 }
